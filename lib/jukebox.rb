7c6a504c60f7096f51a643fdfabaf23d37bd1929
Olga Rosas <olga.rosas@gmail.com>
2019-06-14 17:56:44 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,15 +12,6 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
-#def say_hello(name)
-#  "Hi #{name}!"
-#end
-
-#puts "Enter your name:"
-#users_name = gets.chomp
-
-#puts say_hello(users_name)
-
 def help
     puts "I accept the following commands:"
     puts "- help : displays this help message"
