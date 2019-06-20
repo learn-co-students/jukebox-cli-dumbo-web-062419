099721baf159c8efb1b6361bda64ec9f6f4aabfe
Aliciea D Suechin <adr330@nyu.edu>
2019-06-20 00:57:19 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -14,7 +14,25 @@
 
 def help
   #this method should be the same as in jukebox.rb
-
+  songs  = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+help = [ 
+I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
+]
+puts help 
 end
 
 
@@ -22,8 +40,10 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
+   songs.each_with_index do |item, index|
+    puts "#{index+1}: #{item}"
 end
-
+songs
 
 def play(my_songs)
   #this method is slightly different!
@@ -33,13 +53,28 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+  puts "Please enter a song name or number:"
+  user_response = gets.downcase.chomp
+
+  if (1..9).to_a.include?(user_response.to_i)
+    puts "Playing #{songs[user_response.to_i - 1]}"
+    elsif songs.include?(user_response)
+    puts "Playing #{user_response}"
+  else
+    expect 
+    puts "Invalid input, please try again"
+  end
 end
+play(my_songs)
+end 
+
 
 def exit_jukebox
   #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+  end 
 end
